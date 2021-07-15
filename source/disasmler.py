fa0e7bf140e295a747b0fc7d9fa0f4777e80ead2
Sebastian Kunz <sebi.kunz92@icloud.com>
2021-07-15 12:16:47 +0200

    disasembling with hex rep.

--- source/disasmler.py
+++ source/disasmler.py
@@ -2,43 +2,35 @@ import subprocess
 import os.path
 from log import log
 
-filename = "tmp/prog"
+#Only for Development
+def decompile_without_hex_rep(filename, export):
+    if os.path.isfile(filename):
+        p = subprocess.Popen(["objdump", "-D", "--no-show-raw-insn", filename], stdout=subprocess.PIPE)
+        out, err = p.communicate()
+        out = out.decode("utf-8")
+        if export:
+            f = open("tmp/tmp_wh.asm", "w")
+            f.write(out)
+            f.close()
+        log(os.path.basename(__file__), "{} decompiled sucessfully without hex representation".format(filename))
+        return out
+    else:
+        log(os.path.basename(__file__), "File not found: {}".format(filename))
+        return False
 
-def decompile_without_hex_rep(filename):
-    p = subprocess.Popen(["objdump", "-D", "--no-show-raw-insn", filename], stdout=subprocess.PIPE)
-    out, err = p.communicate()
-    out = out.decode("utf-8")
-    f = open("tmp/tmp.asm", "w")
-    f.write(out)
-    f.close()
-
-def decompile(filename):
+def decompile(filename, export):
     if os.path.isfile(filename):
         subp_command = ["objdump", "-D", filename]
         p = subprocess.Popen(subp_command, stdout=subprocess.PIPE)
         print()
         out, err = p.communicate()
         out = out.decode("utf-8")
-        f = open("tmp/tmp.asm", "w")
-        f.write(out)
-        f.close()
+        if export:
+            f = open("tmp/tmp.asm", "w")
+            f.write(out)
+            f.close()
         log(os.path.basename(__file__), "{} decompiled sucessfully".format(filename))
+        return out
     else:
         log(os.path.basename(__file__), "File not found: {}".format(filename))
-    
-
-def getHEX(startAddress, stopAddress):
-    """Converting the given mach-o executeable and returns the section between start- and stop-address
-
-    Args:
-        startAddress (String): Start-address as hex-value
-        stopAddress ([type]): Stop-address as hex-value
-    """
-    p = subprocess.Popen(["objdump", "-D", "--no-show-raw-insn", filename], stdout=subprocess.PIPE)
-    out, err = p.communicate()
-    out = out.decode("utf-8")
-    f = open("tmp/tmp.asm", "w")
-    f.write(out)
-    f.close()
-
-decompile(filename)
+        return False

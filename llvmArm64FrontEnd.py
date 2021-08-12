8c5194e14a608e274e824b707f2fbbce4b66cc94
Sebastian Kunz <sebi.kunz92@icloud.com>
2021-08-12 15:23:51 +0200

    Changed Location of Main-File

--- llvmArm64FrontEnd.py
+++ llvmArm64FrontEnd.py
@@ -1,5 +1,9 @@
+import sys
+sys.path.insert(1, './source')
+
 import parser
 import disasmler
+import converter
 
 #SETTINGS
 export = False
@@ -9,6 +13,8 @@ def generate_llvm_ir_of_arm64(source, target):
     if asm_raw == False:
         return False
     asm_list = parser.parse_asm(asm_raw)
+    
+    converter.analyze_asm(asm_list)
 
-    #TEST ONLY!
-    return asm_list
+#TEST
+generate_llvm_ir_of_arm64("./tmp/prog", "./tmp/")

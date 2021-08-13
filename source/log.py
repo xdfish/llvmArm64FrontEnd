4db932eabc6f773cac4cd008ddabcd0fedc497e1
Sebastian Kunz <sebi.kunz92@icloud.com>
2021-08-13 09:42:10 +0200

    added doc-strings

--- source/log.py
+++ source/log.py
@@ -3,13 +3,28 @@ import os.path
 
 export_log = False
 
-def log(module, msg):
+def log(module: str, msg: str):
+    """
+    This Function is used for a log-like output with timestamp
+
+    :param module: Name of the module, the log was triggered from
+    :type module: str
+    :param msg: Message for the output
+    :type msg: str
+    """
+    
     text = "{}:\t\t{}\n".format(module, msg)
     print(text)
     if export_log:
         export_log(text)
 
-def export_log(txt):
+def export_log(txt: str):
+    """exports the log output from log() in a text-file.
+
+    :param txt: text added to the file
+    :type txt: str
+    """
+
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     directory = "../tmp"

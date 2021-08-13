e99c0d5d9f9c05c6ca7ecf76acc9fb7a09704bb0
Sebastian Kunz <sebi.kunz92@icloud.com>
2021-08-13 18:59:19 +0200

    updated for testing

--- llvmArm64FrontEnd.py
+++ llvmArm64FrontEnd.py
@@ -12,9 +12,9 @@ def generate_llvm_ir_of_arm64(source, target):
     asm_raw = disasmler.decompile(source, export)
     if asm_raw == False:
         return False
-    asm_list = parser.parse_asm(asm_raw)
+    asm_parsed = parser.parse_asm(asm_raw)
     
-    converter.analyze_asm(asm_list)
+    converter.analyze_asm(asm_parsed)
 
 #TEST
 generate_llvm_ir_of_arm64("./tmp/prog", "./tmp/")

417355c93b4a1bc4275a5b57b8cf51b360a975d6
Sebastian Kunz <sebi.kunz92@icloud.com>
2021-08-16 09:46:25 +0200

    doc-strings added

--- source/disasmler.py
+++ source/disasmler.py
@@ -1,9 +1,21 @@
+from os import strerror
 import subprocess
 import os.path
 from log import log
 
 #Only for Development
-def decompile_without_hex_rep(filename, export):
+def decompile_without_hex_rep(filename: str, export: bool) -> any:
+    """
+    Disasembles an executeable to an assember listing without the hex representation of the Instructions
+    This function is currently not in use for the ztransofmration process.
+
+    :param filename: filename (and path) to the file
+    :type filename: str
+    :param export: if True, the asm_listing ist exported to the tmp folder
+    :type export: bool
+    :return: returns an assembler Listing if successful, or False in case of an error.
+    :rtype: str or Bool(False)
+    """
     if os.path.isfile(filename):
         p = subprocess.Popen(["objdump", "-D", "--no-show-raw-insn", filename], stdout=subprocess.PIPE)
         out, err = p.communicate()
@@ -19,6 +31,16 @@ def decompile_without_hex_rep(filename, export):
         return False
 
 def decompile(filename, export):
+    """
+    Disasembles an executeable to an assembler listing with the hex-representation of the instructions
+
+    :param filename: filename (and path) to the file
+    :type filename: str
+    :param export: if True, the asm_listing ist exported to the tmp folder
+    :type export: bool
+    :return: returns an assembler Listing if successful, or False in case of an error.
+    :rtype: str or Bool(False)
+    """
     if os.path.isfile(filename):
         subp_command = ["objdump", "-D", filename]
         p = subprocess.Popen(subp_command, stdout=subprocess.PIPE)

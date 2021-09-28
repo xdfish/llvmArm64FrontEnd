2f16a99db1b8a86d682cbaded35a122bd7003a84
Sebastian Kunz <sebi.kunz92@icloud.com>
2021-09-28 07:26:20 +0200

    updated for new parser method names

--- llvmArm64FrontEnd.py
+++ llvmArm64FrontEnd.py
@@ -1,19 +1,27 @@
+from source.log import log
 import sys
 sys.path.insert(1, './source')
 
-import parser
-import disasmler
-import converter
+import source.parser as parser
+import source.disasmler as disasembler
+import source.converter as converter
 
 #SETTINGS
-export = False
+export = True
 
-def generate_llvm_ir_of_arm64(source, target):
-    asm_raw = disasmler.decompile(source, export)
+def generate_llvm_ir_of_arm64():
+    log("LLVM-IR FRONT-END FOR ARM64 - VERSION 0.1", "")
+    source_file = input("@-> Enter filename: ")
+    input("@-> Press any key to start disasembling")
+    asm_raw = disasembler.disasembled_raw(source_file, export).disasemble()
+    source_file = input("@-> Press any key to start parssing")
     if asm_raw == False:
         return False
-    asm_parsed = parser.parse_asm(asm_raw)
-    converter.analyze_asm(asm_parsed)
+    asm_parsed = parser.parse_raw_asm(asm_raw)
+    source_file = input("@-> Press any key to start conversiond an ir-generation")
+    converter.analyze_asm(asm_parsed).generate()
 
-#TEST
-generate_llvm_ir_of_arm64("./tmp/prog", "./tmp/")
+def run():
+    generate_llvm_ir_of_arm64()
+
+run()

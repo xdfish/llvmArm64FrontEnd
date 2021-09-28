dec883a129e4ed55f526a008d207128e7b05e190
Sebastian Kunz <sebi.kunz92@icloud.com>
2021-09-28 08:56:10 +0200

    added file export

--- llvmArm64FrontEnd.py
+++ llvmArm64FrontEnd.py
@@ -1,13 +1,20 @@
 from source.log import log
 import sys
+import os
 sys.path.insert(1, './source')
 
 import source.parser as parser
 import source.disasmler as disasembler
 import source.converter as converter
 
+
+VERSION = 0.1
+OUT_FOLDER_NAME = "out"
+
 #SETTINGS
-export = True
+export: bool = True
+filename: str = "out"
+
 
 def generate_llvm_ir_of_arm64():
     log("LLVM-IR FRONT-END FOR ARM64 - VERSION 0.1", "")
@@ -21,9 +28,18 @@ def generate_llvm_ir_of_arm64():
     if not asm_parsed:
         return
     input("@-> Press any key to start conversiond an ir-generation")
-    converter.analyze_asm(asm_parsed).generate()
+    ir_file = converter.analyze_asm(asm_parsed)
+    export_file(filename, ir_file.generate())
 
 def run():
     generate_llvm_ir_of_arm64()
+
+def export_file(filename: str, data: str):
+    #TODO Püfen ob file existiert!
+    path = "../{}/{}.ll".format(OUT_FOLDER_NAME, filename)
+    f = open(filename, 'w')
+    f.write(data)
+    log(os.path.basename(__file__), "llvm ir file saved: {}".format(path))
+    #TODO Püfen ob Datei geschrieben werden konnte
 
 run()

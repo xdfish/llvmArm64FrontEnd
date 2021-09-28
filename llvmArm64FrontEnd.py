4728b0ee3f8f1853401e84fae62e232d1cc6d16f
Sebastian Kunz <sebi.kunz92@icloud.com>
2021-09-28 10:59:08 +0200

    added start arguments

--- llvmArm64FrontEnd.py
+++ llvmArm64FrontEnd.py
@@ -1,6 +1,7 @@
 from source.log import log
 import sys
 import os
+from pathlib import Path
 sys.path.insert(1, './source')
 
 import source.parser as parser
@@ -10,36 +11,96 @@ import source.converter as converter
 
 VERSION = 0.1
 OUT_FOLDER_NAME = "out"
+CFG_START_ARGS_FILE = "config/args.txt"
+CFG_HEAD_FILE = "config/head.txt"
 
 #SETTINGS
 export: bool = True
-filename: str = "out"
-
+inp_filepath: str = None
+out_filename: str = None
+export_unkown_asminstr: bool = False
+information_level_of_detail: int = 0
 
 def generate_llvm_ir_of_arm64():
-    log("LLVM-IR FRONT-END FOR ARM64 - VERSION 0.1", "")
-    source_file = input("@-> Enter filename: ")
-    input("@-> Press any key to start disasembling")
-    asm_raw = disasembler.disasembled_raw(source_file, export).disasemble()
+    global inp_filepath
+    if not inp_filepath:
+        inp_filepath = input("@-> Enter filename(and path): ")
+    input("@-> Press ENTER to start disassembling file: \{}".format(inp_filepath))
+    asm_raw = disasembler.disasembled_raw(inp_filepath, export).disasemble()
     if not asm_raw:
         return
+    inp_filepath = seperate_filename(inp_filepath)
+
     input("@-> Press any key to start parssing")
     asm_parsed = parser.parse_raw_asm(asm_raw)
     if not asm_parsed:
         return
     input("@-> Press any key to start conversiond an ir-generation")
     ir_file = converter.analyze_asm(asm_parsed)
-    export_file(filename, ir_file.generate())
+    export_file(out_filename, ir_file.generate())
 
 def run():
+    start_args = sys.argv
+    if len(start_args) == 2:
+        if start_args[1] == "--version":
+            print("{}\n Version: {}".format(load_cfg_txt(CFG_HEAD_FILE), VERSION))
+            return
+        elif start_args[1] == "--help":
+            print("{}\n{}".format(load_cfg_txt(CFG_HEAD_FILE), load_cfg_txt(CFG_START_ARGS_FILE)))
+            return
+        
+    
+    global out_filename
+    global inp_filepath
+    for i, arg in enumerate(start_args):
+        if i == 1:
+            out_filename = seperate_filename(arg)
+            inp_filepath = arg
+        if arg == "-o":
+                if i != len(start_args) -1:
+                    out_filename = seperate_filename(start_args[i+1])
+                else: 
+                    print('Syntax error: -o needs an additional filename parameter.\nFor example\t-o ex_name\tSet the outputfilename to "ex_name"')
+                    return
+        if arg == "-x":
+            global export_unkown_asminstr
+            export_unkown_asminstr = True
+        if arg == "-d":
+            if i != len(start_args) -1:
+                try:
+                    global information_level_of_detail
+                    information_level_of_detail = int(start_args[i+1])
+                except ValueError:
+                    print("Syntax error: -d needs an additional INTEGER parameter")
+                    return
+            else:
+                print("Syntax error: -d needs an additional parameter between 0 and 3.\nFor example\t-d 0\tDisables the whole information output")
+    print(load_cfg_txt(CFG_HEAD_FILE))
     generate_llvm_ir_of_arm64()
+    
 
 def export_file(filename: str, data: str):
     #TODO Püfen ob file existiert!
-    path = "../{}/{}.ll".format(OUT_FOLDER_NAME, filename)
-    f = open(filename, 'w')
+    if not os.path.exists(OUT_FOLDER_NAME):
+        os.makedirs(OUT_FOLDER_NAME)
+    file_path = "{}/{}.ll".format(OUT_FOLDER_NAME, filename)
+    f = open(file_path, 'w')
     f.write(data)
-    log(os.path.basename(__file__), "llvm ir file saved: {}".format(path))
+    log(os.path.basename(__file__), "llvm ir file saved: {}".format(file_path))
     #TODO Püfen ob Datei geschrieben werden konnte
+
+def seperate_filename(path: str):
+    tmp = path
+    if "/" in path:
+        tmp = tmp.split("/")[-1]
+    if "." in path:
+        tmp = tmp.split(".")[0]
+    return tmp
+
+def load_cfg_txt(cfg_file: str) -> str:
+    if os.path.isfile(cfg_file):
+        return open(cfg_file, 'r').read()
+    else:
+        return "ERROR: NO CONFIG FILE"
 
 run()

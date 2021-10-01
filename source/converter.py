5f2ce056caf3ba639e6b7768a653a1f435a75ba4
Sebastian Kunz <sebi.kunz92@icloud.com>
2021-10-01 18:03:37 +0200

    bundled imports (optical)

--- source/converter.py
+++ source/converter.py
@@ -1,15 +1,7 @@
 import copy as cp
 
-from .parser import asm_inst
-from .parser import asm_function
-from .parser import parsed_asm_list
-from .parser import asm_ptype
-from .parser import asm_dtype
-from .parser import asm_itype
-
-from .irtypes import ir_dtype, ir_fnc_var, ir_var, ir_fnc_ret_var, ir_val, irbb_add, irbb_alloca, irbb_load, irbb_return, irbb_store, irbb_sub, irbb_mul
-from .irtypes import ir_file
-from .irtypes import ir_function
+from .parser import asm_inst, asm_function, parsed_asm_list, asm_ptype, asm_dtype, asm_itype
+from irtypes import ir_file, ir_function, ir_var, ir_val, ir_dtype, ir_fnc_var, ir_fnc_ret_var, irbb_add, irbb_alloca, irbb_load, irbb_return, irbb_store, irbb_sub, irbb_mul
 
 detail_count = 1
 detail_show = True
